52c8af8eca38bc013964cad6a4a73596d67dd14c
ki2098 <1336670149@qq.com>
2025-07-17 16:46:19 +0900

    backup

--- calcM.py
+++ calcM.py
@@ -1,12 +1,17 @@
 import chaospy
+import json
 
 P = 5
 normal = chaospy.Normal()
 psi = chaospy.generate_expansion(P, normal)
+
+lst = []
 
 for i in range(0, P+1):
     for j in range(0, P+1):
         for k in range(0, P+1):
             m = (chaospy.E(psi[i]*psi[j]*psi[k], normal)/chaospy.E(psi[k]*psi[k], normal)).round(5)
             if m != 0:
-                print(f'{i} {j} {k} {m}')
+                lst.append([i, j, k, m])
+
+print(json.dumps(lst))
