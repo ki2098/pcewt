f97043c2cb4deb09b4d416378e5b055b366982be
ki2098 <1336670149@qq.com>
2025-07-28 14:40:46 +0900

    backup

--- pce-lid-cavity/show_result.py
+++ pce-lid-cavity/show_result.py
@@ -20,8 +20,8 @@ mc_u_mean = mc_df['u_mean'].values
 mc_u_mean_along_line = griddata(points, mc_u_mean, plot_points)
 
 plt.figure()
-plt.plot(y_line, pce_u_mean_along_line, label='PCE', linewidth=1)
-plt.scatter(y_line[::3], mc_u_mean_along_line[::3], label='Monte Carlo', marker='+', s=50)
+plt.plot(y_line, pce_u_mean_along_line, label='PCE (P=6)', linewidth=1)
+plt.scatter(y_line[::3], mc_u_mean_along_line[::3], label='Monte Carlo (500 samples)', marker='+', s=50)
 plt.xlabel('y')
 plt.ylabel('E[u]')
 plt.title(f'E[u] along x={x_plot}')
@@ -35,8 +35,8 @@ mc_u_var = mc_df['u_var'].values
 mc_u_var_along_line = griddata(points, mc_u_var, plot_points)
 
 plt.figure()
-plt.plot(y_line, pce_u_var_along_line, label='PCE', linewidth=1, color='red')
-plt.scatter(y_line[::3], mc_u_var_along_line[::3], label='Monte Carlo', marker='+', c='r', s=50)
+plt.plot(y_line, pce_u_var_along_line, label='PCE (P=6)', linewidth=1, color='red')
+plt.scatter(y_line[::3], mc_u_var_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='r', s=50)
 plt.xlabel('y')
 plt.ylabel('Var[u]')
 plt.title(f'Var[u] along x={x_plot}')
@@ -50,8 +50,8 @@ mc_v_mean = mc_df['v_mean'].values
 mc_v_mean_along_line = griddata(points, mc_v_mean, plot_points)
 
 plt.figure()
-plt.plot(y_line, pce_v_mean_along_line, label='PCE', linewidth=1, color='green')
-plt.scatter(y_line[::3], mc_v_mean_along_line[::3], label='Monte Carlo', marker='+', c='g', s=50)
+plt.plot(y_line, pce_v_mean_along_line, label='PCE (P=6)', linewidth=1, color='green')
+plt.scatter(y_line[::3], mc_v_mean_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='g', s=50)
 plt.xlabel('y')
 plt.ylabel('E[v]')
 plt.title(f'E[v] along x={x_plot}')
@@ -65,8 +65,8 @@ mc_v_var = mc_df['v_var'].values
 mc_v_var_along_line = griddata(points, mc_v_var, plot_points)
 
 plt.figure()
-plt.plot(y_line, pce_v_var_along_line, label='PCE', linewidth=1, color='black')
-plt.scatter(y_line[::3], mc_v_var_along_line[::3], label='Monte Carlo', marker='+', c='k', s=50)
+plt.plot(y_line, pce_v_var_along_line, label='PCE (P=6)', linewidth=1, color='black')
+plt.scatter(y_line[::3], mc_v_var_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='k', s=50)
 plt.xlabel('y')
 plt.ylabel('Var[v]')
 plt.title(f'Var[v] along x={x_plot}')
