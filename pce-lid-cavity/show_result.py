302bad8c7b8f09c90a94ce193610e5a20f6840bf
ki2098 <1336670149@qq.com>
2025-07-28 17:50:48 +0900

    backup

--- pce-lid-cavity/show_result.py
+++ pce-lid-cavity/show_result.py
@@ -8,69 +8,100 @@ mc_df = pd.read_csv('data/mc-statistics.csv')
 
 points = pce_df[['x', 'y']].values
 
-x_plot = 0.0
-y_line = np.linspace(pce_df['y'].min(), pce_df['y'].max(), 100)
-x_line = np.full_like(y_line, x_plot)
-plot_points = np.column_stack((x_line, y_line))
+x_plot = 0
+y_plot = 0
+vy = np.linspace(pce_df['y'].min(), pce_df['y'].max(), 100)
+vx = np.full_like(vy, x_plot)
+vline = np.column_stack((vx, vy))
+hx = np.linspace(pce_df['x'].min(), pce_df['x'].max(), 100)
+hy = np.full_like(vx, y_plot)
+hline = np.column_stack((hx, hy))
 
-pce_u_mean = pce_df['u_mean'].values
-pce_u_mean_along_line = griddata(points, pce_u_mean, plot_points)
+def plot_variable(varname: str):
+    pce_var = pce_df[varname].values
+    pce_var_vline = griddata(points, pce_var, vline)
+    pce_var_hline = griddata(points, pce_var, hline)
 
-mc_u_mean = mc_df['u_mean'].values
-mc_u_mean_along_line = griddata(points, mc_u_mean, plot_points)
+    mc_var = mc_df[varname].values
+    mc_var_vline = griddata(points, mc_var, vline)
+    mc_var_hline = griddata(points, mc_var, hline)
 
-plt.figure()
-plt.plot(y_line, pce_u_mean_along_line, label='PCE (P=6)', linewidth=1)
-plt.scatter(y_line[::3], mc_u_mean_along_line[::3], label='Monte Carlo (500 samples)', marker='+', s=50)
-plt.xlabel('y')
-plt.ylabel('E[u]')
-plt.title(f'E[u] along x={x_plot}')
-plt.grid(True)
-plt.legend()
+    plt.figure()
+    plt.plot(vy, pce_var_vline, label=f'PCE (P=6) along x = {x_plot}', linewidth=1, color='blue')
+    plt.plot(hx, pce_var_hline, label=f'PCE (P=6) along y = {y_plot}', linewidth=1, color='blue', ls=':')
+    plt.scatter(vy[::3], mc_var_vline[::3], label=f'Monte Carlo (500 samples) along x = {x_plot}', marker='+', s=50, c='red')
+    plt.scatter(hx[::3], mc_var_hline[::3], label=f'Monte Carlo (500 samples) along y = {y_plot}', marker='.', c='red')
+    plt.xlabel('x or y')
+    plt.ylabel(varname)
+    plt.title(f'{varname} along line x={x_plot} and line y={y_plot}')
+    plt.grid(True)
+    plt.legend()
 
-pce_u_var = pce_df['u_var'].values
-pce_u_var_along_line = griddata(points, pce_u_var, plot_points)
+plot_variable('E[u]')
+plot_variable('Var[u]')
+plot_variable('E[v]')
+plot_variable('Var[v]')
 
-mc_u_var = mc_df['u_var'].values
-mc_u_var_along_line = griddata(points, mc_u_var, plot_points)
+# pce_u_mean = pce_df['u_mean'].values
+# pce_u_mean_vline = griddata(points, pce_u_mean, vline)
+# pce_u_mean_hline = griddata(points, pce_u_mean, hline)
 
-plt.figure()
-plt.plot(y_line, pce_u_var_along_line, label='PCE (P=6)', linewidth=1, color='red')
-plt.scatter(y_line[::3], mc_u_var_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='r', s=50)
-plt.xlabel('y')
-plt.ylabel('Var[u]')
-plt.title(f'Var[u] along x={x_plot}')
-plt.grid(True)
-plt.legend()
+# mc_u_mean = mc_df['u_mean'].values
+# mc_u_mean_vline = griddata(points, mc_u_mean, vline)
+# mc_u_mean_hline = griddata(points, )
 
-pce_v_mean = pce_df['v_mean'].values
-pce_v_mean_along_line = griddata(points, pce_v_mean, plot_points)
+# plt.figure()
+# plt.plot(y_line, pce_u_mean_along_line, label='PCE (P=6)', linewidth=1)
+# plt.scatter(y_line[::3], mc_u_mean_along_line[::3], label='Monte Carlo (500 samples)', marker='+', s=50)
+# plt.xlabel('y')
+# plt.ylabel('E[u]')
+# plt.title(f'E[u] along x={x_plot}')
+# plt.grid(True)
+# plt.legend()
 
-mc_v_mean = mc_df['v_mean'].values
-mc_v_mean_along_line = griddata(points, mc_v_mean, plot_points)
+# pce_u_var = pce_df['u_var'].values
+# pce_u_var_along_line = griddata(points, pce_u_var, plot_points)
 
-plt.figure()
-plt.plot(y_line, pce_v_mean_along_line, label='PCE (P=6)', linewidth=1, color='green')
-plt.scatter(y_line[::3], mc_v_mean_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='g', s=50)
-plt.xlabel('y')
-plt.ylabel('E[v]')
-plt.title(f'E[v] along x={x_plot}')
-plt.grid(True)
-plt.legend()
+# mc_u_var = mc_df['u_var'].values
+# mc_u_var_along_line = griddata(points, mc_u_var, plot_points)
 
-pce_v_var = pce_df['v_var'].values
-pce_v_var_along_line = griddata(points, pce_v_var, plot_points)
+# plt.figure()
+# plt.plot(y_line, pce_u_var_along_line, label='PCE (P=6)', linewidth=1, color='red')
+# plt.scatter(y_line[::3], mc_u_var_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='r', s=50)
+# plt.xlabel('y')
+# plt.ylabel('Var[u]')
+# plt.title(f'Var[u] along x={x_plot}')
+# plt.grid(True)
+# plt.legend()
 
-mc_v_var = mc_df['v_var'].values
-mc_v_var_along_line = griddata(points, mc_v_var, plot_points)
+# pce_v_mean = pce_df['v_mean'].values
+# pce_v_mean_along_line = griddata(points, pce_v_mean, plot_points)
 
-plt.figure()
-plt.plot(y_line, pce_v_var_along_line, label='PCE (P=6)', linewidth=1, color='black')
-plt.scatter(y_line[::3], mc_v_var_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='k', s=50)
-plt.xlabel('y')
-plt.ylabel('Var[v]')
-plt.title(f'Var[v] along x={x_plot}')
-plt.grid(True)
-plt.legend()
+# mc_v_mean = mc_df['v_mean'].values
+# mc_v_mean_along_line = griddata(points, mc_v_mean, plot_points)
+
+# plt.figure()
+# plt.plot(y_line, pce_v_mean_along_line, label='PCE (P=6)', linewidth=1, color='green')
+# plt.scatter(y_line[::3], mc_v_mean_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='g', s=50)
+# plt.xlabel('y')
+# plt.ylabel('E[v]')
+# plt.title(f'E[v] along x={x_plot}')
+# plt.grid(True)
+# plt.legend()
+
+# pce_v_var = pce_df['v_var'].values
+# pce_v_var_along_line = griddata(points, pce_v_var, plot_points)
+
+# mc_v_var = mc_df['v_var'].values
+# mc_v_var_along_line = griddata(points, mc_v_var, plot_points)
+
+# plt.figure()
+# plt.plot(y_line, pce_v_var_along_line, label='PCE (P=6)', linewidth=1, color='black')
+# plt.scatter(y_line[::3], mc_v_var_along_line[::3], label='Monte Carlo (500 samples)', marker='+', c='k', s=50)
+# plt.xlabel('y')
+# plt.ylabel('Var[v]')
+# plt.title(f'Var[v] along x={x_plot}')
+# plt.grid(True)
+# plt.legend()
 
 plt.show()
